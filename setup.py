9022eb5849710a0149c7b3ad0308b6d25e7540dc
csjapic <csjapic@gmail.com>
2024-07-02 16:31:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+from setuptools import setup
 
+setup(
+   name='BookLover',
+	    version='0.1',
+	    url='https://github.com/csjapic/practice-package/tree/e82ae4091ccfe25ea6dec1b1a22029f35d676937/package',
+	    author='Chloe Japic',
+	    author_email='csjapic@gmail.com',
+        license='MIT',
+	    description='Description of my package',
+	    packages=['BookLover']    
+    
+)
